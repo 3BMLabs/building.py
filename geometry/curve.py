386eabfc115c7bcfb74a97510ab0650fffc73234
Devtec <93218222+UserDevtec@users.noreply.github.com>
2023-03-14 21:49:26 +0100

    Update curve.py

--- geometry/curve.py
+++ geometry/curve.py
@@ -39,6 +39,9 @@ sys.path.append(str(package_root_directory))
 
 from geometry.point import *
 from packages import helper
+from abstract.vector import Vector3
+from abstract.plane import Plane
+from specklepy.objects.primitive import Interval as SpeckleInterval #temp
 
 
 class Line:
@@ -147,19 +150,86 @@ class PolyGon:
 
 
 class Arc:
-    def __init__(self, radius, startAngle, endAngle, angleRadians, plane, startPoint, midPoint, endPoint, length, id=helper.generateID()) -> None:
-        self.radius = radius
-        self.startAngle = startAngle
-        self.endAngle = endAngle
-        self.angleRadians = angleRadians
-        self.plane = plane
+    def __init__(self, startPoint: Point, midPoint: Point, endPoint: Point):
         self.startPoint = startPoint
         self.midPoint = midPoint
         self.endPoint = endPoint
-        self.length = length
-        self.id = id
-        pass #Curve
+        self.plane = Plane(
+            origin=Point.from_coords((startPoint.x + endPoint.x) / 2, (startPoint.y + endPoint.y) / 2, (startPoint.z + endPoint.z) / 2),
+            normal=Vector3.from_coords(0, 0, 1),
+            xdir=Vector3.from_coords(1, 0, 0),
+            ydir=Vector3.from_coords(0, 1, 0)
+        )
+        self.radius=self.radius()
+        self.startAngle=0
+        self.endAngle=0
+        self.angleRadians=0
+        self.area=0
+        self.length=self.length()
+        self.units="mm"
+        # self.id = id
 
+    def distance(self, p1, p2):
+        return math.sqrt((p2.x-p1.x)**2 + (p2.y-p1.y)**2 + (p2.z-p1.z)**2)
+    
+    def radius(self):
+        a = self.distance(self.startPoint, self.midPoint)
+        b = self.distance(self.midPoint, self.endPoint)
+        c = self.distance(self.endPoint, self.startPoint)
+        s = (a + b + c) / 2
+        A = math.sqrt(s * (s-a) * (s-b) * (s-c))
+        R = (a * b * c) / (4 * A)
+        return R
+
+    def length(self):
+        x1, y1, z1 = self.startPoint.x, self.startPoint.y, self.startPoint.z
+        x2, y2, z2 = self.midPoint.x, self.midPoint.y, self.midPoint.z
+        x3, y3, z3 = self.endPoint.x, self.endPoint.y, self.endPoint.z
+
+        r1 = ((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)**0.5 / 2
+        a = math.sqrt((x2-x1)**2+(y2-y1)**2+(z2-z1)**2)
+        b = math.sqrt((x3-x2)**2+(y3-y2)**2+(z3-z2)**2)
+        c = math.sqrt((x3-x1)**2+(y3-y1)**2+(z3-z1)**2)
+        cos_hoek = (a**2 + b**2 - c**2) / (2*a*b)
+        m1 = math.acos(cos_hoek)
+        arc_length = r1 * m1
+
+        return arc_length
+
+
+    @classmethod
+    def ByThreePoints(self, startPoint: Point, midPoint: Point, endPoint: Point, plane=None):
+        radius = self(startPoint=startPoint, midPoint=midPoint, endPoint=endPoint).radius
+        startAngle = self(startPoint=startPoint, midPoint=midPoint, endPoint=endPoint).startAngle
+        endAngle = self(startPoint=startPoint, midPoint=midPoint, endPoint=endPoint).endAngle
+        angleRadians = self(startPoint=startPoint, midPoint=midPoint, endPoint=endPoint).angleRadians
+        area = self(startPoint=startPoint, midPoint=midPoint, endPoint=endPoint).area
+        length = self(startPoint=startPoint, midPoint=midPoint, endPoint=endPoint).length
+        units = self(startPoint=startPoint, midPoint=midPoint, endPoint=endPoint).units
+
+        if plane is None:
+            plane = Plane(
+                origin=Point.from_coords((startPoint.x + endPoint.x) / 2, (startPoint.y + endPoint.y) / 2, (startPoint.z + endPoint.z) / 2),
+                normal=Vector3.from_coords(0, 0, 1),
+                xdir=Vector3.from_coords(1, 0, 0),
+                ydir=Vector3.from_coords(0, 1, 0)
+            )
+        
+        return Arc(
+            startPoint=startPoint,
+            midPoint=midPoint,
+            endPoint=endPoint,
+            domain=SpeckleInterval(start=0, end=1), #create IntervalToSpeckleInterval in 'speckle.py'
+            plane=plane,
+            radius=radius,
+            startAngle=startAngle,
+            endAngle=endAngle,
+            angleRadians=angleRadians,
+            area=area,
+            length=length,
+            units=units
+        )
+    
     def __id__(self):
         return f"id:{self.id}"
 
