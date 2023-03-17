fdefc41357b0a3d08cc0e35ed0cb3e519c215466
joas1606 <j.a.hollander03@gmail.com>
2023-03-17 16:47:37 +0100

    edits

--- temp/pil_img.py
+++ temp/pil_img.py
@@ -1,18 +1,28 @@
 from exchange.speckle import *
 from PIL import Image
-from objects.shape import *
-from objects.frame import *
 
 
-square = Frame.byStartpointEndpoint(Point(0, 0, 0), Point(1, 0, 0), Rectangle("joas", 0, 1).curve, "L-frame")
-square2 = Frame.byStartpointEndpoint(Point(1, 0, 0), Point(2, 0, 0), Rectangle("joas", 0, 1).curve, "L-frame")
-square3 = Frame.byStartpointEndpoint(Point(2, 0, 0), Point(3, 0, 0), Rectangle("joas", 0, 1).curve, "L-frame")
-
-
-img = Image.open("rgb.png")
+img = Image.open("photo.jpg")
 pixels = img.load()
+
+
+facenr = -1
+numberofpixels = 0
+vertx = 0
+verty = 0
+vertz = 0
+
+
 kleurcode_rijen = []
 pixellist = []
+vert = []
+faces = []
+colorlst = []
+
+
+def rgb_to_int(rgb):
+    r, g, b = [max(0, min(255, c)) for c in rgb]
+    return (255 << 24) | (r << 16) | (g << 8) | b
 
 
 for y in range(img.height):
@@ -27,8 +37,53 @@ for kleurcode_rij in kleurcode_rijen:
         pixellist.append(kleurcode)
 
 
-print(pixellist)
+for i in pixellist:
+    faces.append(4)
+    vert.append(vertx)
+    vert.append(verty)
+    vert.append(vertz)
+    facenr += 1
+    faces.append(facenr)
+    vertx += 1
+    vert.append(vertx)
+    vert.append(verty)
+    vert.append(vertz)
+    facenr += 1
+    faces.append(facenr)
+    verty -= 1
+    vert.append(vertx)
+    vert.append(verty)
+    vert.append(vertz)
+    facenr += 1
+    faces.append(facenr)
+    vertx -= 1
+    vert.append(vertx)
+    vert.append(verty)
+    vert.append(vertz)
+    facenr += 1
+    faces.append(facenr)
+    vertx += 1
+    verty += 1
+    numberofpixels += 1
+
+    if numberofpixels == len(rij_kleurcodes):
+        vertx = 0
+        verty -= 1
+        numberofpixels = 0
 
 
-SpeckleObj = translateObjectsToSpeckleObjects([square, square2, square3])
+for i in pixellist:
+    argbint_color = rgb_to_int(i)
+    colorlst.append(argbint_color)
+    colorlst.append(argbint_color)
+    colorlst.append(argbint_color)
+    colorlst.append(argbint_color)
+
+
+def SpeckleMeshByImage(verts, faces, name):
+    spcklmesh = SpeckleMesh(vertices=verts, faces=faces, name=name, colors=colorlst)
+    return spcklmesh
+
+
+SpeckleObj = [SpeckleMeshByImage(vert, faces, "First img")]
 Commit = TransportToSpeckle("speckle.xyz", "8136460d9e", SpeckleObj, "Shiny Commit")
