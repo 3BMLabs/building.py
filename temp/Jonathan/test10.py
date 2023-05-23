adfda6a5b860eda7e87a5c36e2892fc02f0360e7
Devtec <info@devtec.nl>
2023-05-23 14:32:22 +0200

    update

---
+++ temp/Jonathan/test10.py
@@ -0,0 +1,30 @@
+import sys, math, requests, json
+from svg.path import parse_path
+from typing import List, Tuple
+from pathlib import Path
+# https://help.xtools.pro/pro/22.0/en/XTools_Pro_Components/Geometry_Tools/Split_Polygons.htm
+
+sys.path.append(str(Path(__file__).resolve().parents[2]))
+
+
+from exchange.speckle import TransportToSpeckle, translateObjectsToSpeckleObjects
+from geometry.point import Point
+from geometry.curve import *
+from abstract.vector import Vector3
+from abstract.intersect2d import *
+from abstract.plane import Plane
+from abstract.text import Text
+from abstract.intersect2d import Intersect2d
+from objects.datum import *
+from geometry.solid import Extrusion
+from geometry.surface import *
+from objects.objectcollection import *
+from project.fileformat import *
+from objects.shape3d import Origin
+from exchange.DXF import ReadDXF
+
+
+Text2 = Text(text="TAG Center", font_family="arial", bounding_box=False, xyz=Point(0,-6000, 5), rotation=0)
+project.objects.append(Text2)
+
+project.toSpeckle("5ab2faedba")
