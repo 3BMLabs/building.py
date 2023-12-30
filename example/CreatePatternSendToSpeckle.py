ed4865989789a5be839117c6621bd8bee7b2502f
Maarten Vroegindeweij <30430941+DutchSailor@users.noreply.github.com>
2023-12-30 23:01:00 +0100

    Extention of PAT-class

--- example/CreatePatternSendToSpeckle.py
+++ example/CreatePatternSendToSpeckle.py
@@ -1,7 +1,49 @@
 from exchange.pat import *
+from geometry.curve import Line, Point
+from project.fileformat import BuildingPy
+import sys
+
+project = BuildingPy("Test patterns","0")
+project.speckleserver = "speckle.xyz"
 
 pat1 = PAT().TilePattern("500x500", 500, 500, Revitmodelpattern)
 pat2 = PAT().BlockPattern("Blokpatroon",300,4,Revitmodelpattern)
 pat3 = PAT().ParallelLines("Bamboe",[0,150,100,100,50,150,50,100,100],Revitmodelpattern)
 
-CreatePatFile([pat1,pat2,pat3],'C:/TEMP/test3.pat')
+#CreatePatFile([pat1,pat2,pat3],'C:/TEMP/test3.pat')
+
+# rules: ;;;angle, x-origin, y-origin, shift_pattern, offset(spacing), pen_down, pen_up (negatief waarde)
+
+test = PATRow().create(0,0,0,0,250,0,0)
+test = PATRow().create(0,0,0,0,250,0,0)
+def PatRowGeom(patrow: PATRow, width: float, height: float):
+    nlines = int(height /  patrow.offset_spacing)
+    lines = []
+    n = 0
+    for i in range(nlines):
+        if patrow.dash == 0 and patrow.space == 00:
+            x_start = patrow.x_orig + patrow.shift_pattern
+            y_start = patrow.y_orig + n * patrow.offset_spacing
+            x_end = width + patrow.shift_pattern
+            y_end = patrow.y_orig + math.tan(math.radians(patrow.angle))*width + n * patrow.offset_spacing
+            lines.append(Line(Point(x_start,y_start,0),Point(x_end,y_end,0)))
+            n = n + 1
+    return lines
+
+lines = PatRowGeom(test,2000,1000)
+
+for i in lines:
+    project.objects.append(i)
+
+project.toSpeckle("3e34ec62e2")
+
+
+#self.angle = 0
+#self.x_orig = 0
+#self.y_orig = 0
+#self.shift_pattern = 0
+#self.offset = 0
+
+#self.dash = 0
+#self.space = 0
+#self.patstr = ""
--- exchange/pat.py
+++ exchange/pat.py
@@ -44,14 +44,14 @@ class PATRow:
         self.x_orig = 0
         self.y_orig = 0
         self.shift_pattern = 0
-        self.offset = 0
+        self.offset_spacing = 0
         self.dash = 0
         self.space = 0
         self.patstr = ""
 
     def createpatstr(self):
         patstr = str(self.angle) + ",  " + str(self.x_orig) + ",  " + str(self.y_orig) + ",  " + str(self.shift_pattern) + ",  " + str(
-            self.offset)
+            self.offset_spacing)
         if self.dash == 0:
             addstr = ""
         else:
@@ -59,12 +59,14 @@ class PATRow:
         patstr = patstr + addstr
         return patstr
 
-    def create(self,angle: float, x_orig: float, y_orig: float, shift_pattern: float, offset: float, dash: float=0, space: float=0):
+    def create(self,angle: float, x_orig: float, y_orig: float, shift_pattern: float, offset_spacing: float, dash: float=0, space: float=0):
+        # if dash and space are 0 then no pattern
+        # rules: ;;;angle, x-origin, y-origin, shift_pattern, offset(spacing), pen_down, pen_up (negatief waarde)
         self.angle = angle
         self.x_orig = x_orig
         self.y_orig = y_orig
         self.shift_pattern = shift_pattern
-        self.offset = offset
+        self.offset_spacing = offset_spacing
         self.dash = dash
         self.space = space
         self.patstr = self.createpatstr()
@@ -92,6 +94,7 @@ class PAT:
         self.patstrings.append(row2.patstr)
         self.patstrings.append(";")
         return self
+
     def BlockPattern(self, name: str, grosswidthheight: float, numbersublines: int, patterntype: str):
         #this is a block pattern
         subspacing = grosswidthheight / numbersublines
@@ -116,6 +119,136 @@ class PAT:
             self.patstrings.append(row3.patstr)
             self.patstrings.append(row4.patstr)
             n = n + 1
+        self.patstrings.append(";")
+        return self
+
+    def CombiPattern(self, name: str, grosswidthheight: float, patterntype: str):
+        #this is a combined pattern
+        width = (2 / 3) * grosswidthheight
+        t = grosswidthheight / 6
+        self.name = name
+        self.patterntype = patterntype
+
+        row1 = PATRow().create(0, 0, 0, 0, grosswidthheight, width, (-2 * t))
+        row2 = PATRow().create(0, 0, t, 0, grosswidthheight, width, (-2 * t))
+        row3 = PATRow().create(0, 0, 2 * t, 0, grosswidthheight, width, (-2 * t))
+        row4 = PATRow().create(0, 2 * t, width, 0, grosswidthheight, width, (-2 * t))
+        row5 = PATRow().create(0, 2 * t, width + t, 0, grosswidthheight, width, (-2 * t))
+        row6 = PATRow().create(0, 2 * t, width + 2 * t, 0, grosswidthheight, width, (-2 * t))
+        row7 = PATRow().create(90, 0, 2 * t, 0, grosswidthheight, width, (-2 * t))
+        row8 = PATRow().create(90, t, 2 * t, 0, grosswidthheight, width, (-2 * t))
+        row9 = PATRow().create(90, 2 * t, 2 * t, 0, grosswidthheight, width, (-2 * t))
+        row10 = PATRow().create(90, width, 0, 0, grosswidthheight, width, (-2 * t))
+        row11 = PATRow().create(90, width + t, 0, 0, grosswidthheight, width, (-2 * t))
+        row12 = PATRow().create(90, width + 2 * t, 0, 0, grosswidthheight, width, (-2 * t))
+
+        self.patrows.append(row1)
+        self.patrows.append(row2)
+        self.patrows.append(row3)
+        self.patrows.append(row4)
+        self.patrows.append(row5)
+        self.patrows.append(row6)
+        self.patrows.append(row7)
+        self.patrows.append(row8)
+        self.patrows.append(row9)
+        self.patrows.append(row10)
+        self.patrows.append(row11)
+        self.patrows.append(row12)
+
+        self.patstrings.append("*" + name)
+        self.patstrings.append(patterntype)
+        self.patstrings.append(row1.patstr)
+        self.patstrings.append(row2.patstr)
+        self.patstrings.append(row3.patstr)
+        self.patstrings.append(row4.patstr)
+        self.patstrings.append(row5.patstr)
+        self.patstrings.append(row6.patstr)
+        self.patstrings.append(row7.patstr)
+        self.patstrings.append(row8.patstr)
+        self.patstrings.append(row9.patstr)
+        self.patstrings.append(row10.patstr)
+        self.patstrings.append(row11.patstr)
+        self.patstrings.append(row12.patstr)
+        self.patstrings.append(";")
+        return self
+
+    def ChevronPattern(self, name: str, grosswidth: float, widthtile: float, patterntype: str):
+        #this is a chevronpattern(hungarian)
+        lengthline = math.sqrt(2) * grosswidth
+        self.name = name
+        self.patterntype = patterntype
+
+        row1 = PATRow().create(90, 0, 0, 0, grosswidth, 0, 0)
+        row2 = PATRow().create(45, 0, 0, widthtile, widthtile, lengthline, -lengthline)
+        row3 = PATRow().create(-45, -grosswidth, 0, -widthtile, widthtile, lengthline, -lengthline)
+
+        self.patrows.append(row1)
+        self.patrows.append(row2)
+        self.patrows.append(row3)
+
+        self.patstrings.append("*" + name)
+        self.patstrings.append(patterntype)
+        self.patstrings.append(row1.patstr)
+        self.patstrings.append(row2.patstr)
+        self.patstrings.append(row3.patstr)
+        self.patstrings.append(";")
+        return self
+
+    def HerringbonePattern(self, name: str, lengthtile: float, numberOfTilesInLength: float, patterntype: str):
+        # this is a herringbone pattern(visgraat)
+        width = lengthtile / numberOfTilesInLength
+        self.name = name
+        self.patterntype = patterntype
+
+        row1 = PATRow().create(45, 0, 0, width, width, lengthtile + width, -(lengthtile - width))
+        row2 = PATRow().create(135, 0, 0, -width, width, lengthtile, -lengthtile)
+        row3 = PATRow().create(-45, 0, 0, -width, width, width, -(2 * lengthtile - width))
+
+        self.patrows.append(row1)
+        self.patrows.append(row2)
+        self.patrows.append(row3)
+
+        self.patstrings.append("*" + name)
+        self.patstrings.append(patterntype)
+        self.patstrings.append(row1.patstr)
+        self.patstrings.append(row2.patstr)
+        self.patstrings.append(row3.patstr)
+        self.patstrings.append(";")
+        return self
+
+    def Strips(self, name: str, spacing: float, angle: float, patterntype: str):
+        # these are continues lines with a certain spacing and angle
+        self.name = name
+        self.patterntype = patterntype
+
+        row1 = PATRow().create(angle, 0, 0, 0, spacing, 0, 0)
+
+        self.patrows.append(row1)
+
+        self.patstrings.append("*" + name)
+        self.patstrings.append(patterntype)
+        self.patstrings.append(row1.patstr)
+        self.patstrings.append(";")
+        return self
+
+    def StretcherBondPattern(self, name: str, width: float, height: float, shift: float, patterntype: str):
+        # this is a brick pattern rectangle with a shift
+        self.name = name
+        self.patterntype = patterntype
+
+        row1 = PATRow().create(0, 0, 0, 0, height, 0, 0)
+        row2 = PATRow().create(90, 0, 0, 0, width, height, -height)
+        row3 = PATRow().create(90, shift, height, 0, width, height, -height)
+
+        self.patrows.append(row1)
+        self.patrows.append(row2)
+        self.patrows.append(row3)
+
+        self.patstrings.append("*" + name)
+        self.patstrings.append(patterntype)
+        self.patstrings.append(row1.patstr)
+        self.patstrings.append(row2.patstr)
+        self.patstrings.append(row3.patstr)
         self.patstrings.append(";")
         return self
 
@@ -151,116 +284,6 @@ def CreatePatFile(patternobjects: list, filepath: str):
         fp.write(i)
     fp.close()
     return filepath
-
-#Old , to be removed after transition to class system
-def PatRow(angle: float, x_orig: float, y_orig: float, shift_pattern: float, offset: float, dash: float=0, space: float=0):
-    #if dash and space are 0 then no pattern
-    #rules: ;;;angle, x-origin, y-origin, shift_pattern, offset(spacing), pen_down, pen_up (negatief waarde)
-
-    patstr = str(angle) + ",  " + str(x_orig) + ",  " + str(y_orig) + ",  " + str(shift_pattern) + ",  " + str(offset)
-    if dash == 0:
-        addstr = ""
-    else:
-        addstr = ",  " + str(dash) + ",  " + str(space)
-    patstr = patstr + addstr
-    return patstr
-
-def TilePattern(name: str, width: float, height: float, patterntype: str):
-    #this is rectangle tile pattern
-    patstrings = []
-    patstrings.append("*" + name)
-    patstrings.append(patterntype)
-    patstrings.append(PatRow(0,0,0,0,height,0,0))
-    patstrings.append(PatRow(90,0,0,0,width,0,0))
-    patstrings.append(";")
-    return patstrings
-
-def BlockPattern(name: str, grosswidthheight: float, numbersublines: int, patterntype: str):
-    #this is a block pattern
-
-    patstrings = []
-    subspacing = grosswidthheight/numbersublines
-    patstrings.append("*" + name)
-    patstrings.append(patterntype)
-    patstrings.append(PatRow(0,0,0,0,grosswidthheight,0,0))
-    patstrings.append(PatRow(90,0,0,0,grosswidthheight,0,0))
-    n = 0
-    for i in range(numbersublines):
-        patstrings.append(PatRow(0, grosswidthheight,subspacing * n , grosswidthheight, grosswidthheight, grosswidthheight, -grosswidthheight))
-        patstrings.append(PatRow(90, subspacing * n, 0, grosswidthheight, grosswidthheight, grosswidthheight, -grosswidthheight))
-        n = n + 1
-    patstrings.append(";")
-    return patstrings
-
-def CombiPattern(name: str, grosswidthheight: float, patterntype: str):
-    #this is a combined pattern
-    width = (2/3)*grosswidthheight
-    t = grosswidthheight/6
-
-    patstrings = []
-    patstrings.append("*" + name)
-    patstrings.append(patterntype)
-    patstrings.append(PatRow(0, 0, 0, 0, grosswidthheight, width, (-2 * t)))
-    patstrings.append(PatRow(0, 0, t, 0, grosswidthheight, width, (-2 * t)))
-    patstrings.append(PatRow(0, 0, 2 * t, 0, grosswidthheight, width, (-2 * t)))
-    patstrings.append(PatRow(0, 2 * t, width, 0, grosswidthheight, width, (-2 * t)))
-    patstrings.append(PatRow(0, 2 * t, width + t, 0, grosswidthheight, width, (-2 * t)))
-    patstrings.append(PatRow(0, 2 * t, width + 2*t, 0, grosswidthheight, width, (-2 * t)))
-    patstrings.append(PatRow(90, 0, 2 * t, 0, grosswidthheight, width, (-2 * t)))
-    patstrings.append(PatRow(90, t, 2 * t, 0, grosswidthheight, width, (-2 * t)))
-    patstrings.append(PatRow(90, 2 * t, 2 * t, 0, grosswidthheight, width, (-2 * t)))
-    patstrings.append(PatRow(90, width, 0, 0, grosswidthheight, width, (-2 * t)))
-    patstrings.append(PatRow(90, width + t, 0, 0, grosswidthheight, width, (-2 * t)))
-    patstrings.append(PatRow(90, width + 2* t, 0, 0, grosswidthheight, width, (-2 * t)))
-    patstrings.append(";")
-    return patstrings
-
-def ChevronPattern(name: str, grosswidth: float, widthtile: float, patterntype: str):
-    #this is a chevronpattern(hungarian)
-    lengthline = math.sqrt(2)*grosswidth
-
-    patstrings = []
-    patstrings.append("*" + name)
-    patstrings.append(patterntype)
-    patstrings.append(PatRow(90, 0, 0, 0, grosswidth, 0, 0 ))
-    patstrings.append(PatRow(45, 0, 0, widthtile, widthtile,lengthline,-lengthline))
-    patstrings.append(PatRow(-45, -grosswidth, 0, -widthtile , widthtile, lengthline, -lengthline))
-    patstrings.append(";")
-    return patstrings
-
-def HerringbonePattern(name: str, lengthtile: float, numberOfTilesInLength: float, patterntype: str):
-    #this is a herringbone pattern(visgraat)
-    width = lengthtile / numberOfTilesInLength
-
-    patstrings = []
-    patstrings.append("*" + name)
-    patstrings.append(patterntype)
-    patstrings.append(PatRow(45,0,0,width,width,lengthtile+width,-(lengthtile-width)))
-    patstrings.append(PatRow(135,0,0,-width,width,lengthtile,-lengthtile))
-    patstrings.append(PatRow(-45, 0, 0, -width, width, width, -(2*lengthtile-width)))
-    patstrings.append(";")
-    return patstrings
-
-
-def Strips(name: str, spacing: float, angle: float, patterntype: str):
-    #these are continues lines with a certain spacing and angle
-    patstrings = []
-    patstrings.append("*" + name)
-    patstrings.append(patterntype)
-    patstrings.append(PatRow(angle,0,0,0,spacing,0,0))
-    patstrings.append(";")
-    return patstrings
-
-def StretcherBondPattern(name, width, height, shift, patterntype: str):
-    # this is a brick pattern rectangle with a shift
-    patstrings = []
-    patstrings.append("*" + name)
-    patstrings.append(patterntype)
-    patstrings.append(PatRow(0, 0, 0, 0, height, 0, 0))
-    patstrings.append(PatRow(90, 0, 0, 0, width, height, -height))
-    patstrings.append(PatRow(90, shift, height, 0, width, height, -height))
-    patstrings.append(";")
-    return patstrings
 
 #reader
 #drawsection
