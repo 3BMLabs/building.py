515224ef0a8b033fb6793311c7b2f89052454391
Devtec <info@devtec.nl>
2024-02-27 16:15:15 +0100

    update

--- sandbox/IFC/ifc_create_columns.py
+++ sandbox/IFC/ifc_create_columns.py
@@ -31,7 +31,7 @@ run("aggregate.assign_object", model, relating_object=site, product=building)
 run("aggregate.assign_object", model, relating_object=building, product=storey)
 
 
-def place_structural_element(model, from_point:Point, to_point:Point, element_type:str, profilename:str, materialname=None) -> object:
+def place_structural_element(model, from_point:Point, to_point:Point, element_type:str, profilename:str, materialname=None, elementname=None) -> object:
     ifc_class_type = "IfcColumnType" if element_type.lower() == "column" else "IfcBeamType"
     ifc_class = "IfcColumn" if element_type.lower() == "column" else "IfcBeam"
 
@@ -50,24 +50,24 @@ def place_structural_element(model, from_point:Point, to_point:Point, element_ty
     distance = Point.distance(from_point, to_point)
 
     x_rad = math.atan2(distance, (to_point.z-from_point.z))
-    print(x_rad)
     x_deg = math.degrees(x_rad)-90
     print(x_deg)
     
 
-    column_type = run("root.create_entity", model, ifc_class=ifc_class_type, name="C2")
+    column_type = run("root.create_entity", model, ifc_class=ifc_class_type, name=elementname)
     matrix = np.eye(4)
 
     matrix = ifcopenshell.util.placement.rotation(90, "X") @ matrix
     matrix = ifcopenshell.util.placement.rotation(90, "Z") @ matrix
-    # matrix = ifcopenshell.util.placement.rotation(0, "Y") @ matrix -> direction works
 
-    matrix = ifcopenshell.util.placement.rotation(35, "Y") @ matrix
+    # matrix = ifcopenshell.util.placement.rotation(0, "Y") @ matrix -> direction works
+    # matrix = ifcopenshell.util.placement.rotation(180-155, "Y") @ matrix
+
     matrix = ifcopenshell.util.placement.rotation(atvalue_deg, "Z") @ matrix
 
     matrix[:,3][0:3] = (P1[0], P1[1], P1[2])
 
-    material_set = run("material.add_material_set", model, name="C2", set_type="IfcMaterialProfileSet")
+    material_set = run("material.add_material_set", model, name=elementname, set_type="IfcMaterialProfileSet")
     steel = run("material.add_material", model, name=materialname, category="steel")
     shape_profile = model.create_entity("IfcIShapeProfileDef", ProfileName=profilename, ProfileType="AREA", OverallWidth=100, OverallDepth=96, WebThickness=5, FlangeThickness=8, FilletRadius=12, )
     run("material.add_profile", model, profile_set=material_set, material=steel, profile=shape_profile)
@@ -147,7 +147,7 @@ run("geometry.assign_representation", model, product=column, representation=repr
 run("spatial.assign_container", model, relating_structure=storey, product=column)
 
 
-place_structural_element(model, Point.from_matrix(P1), Point.from_matrix(P2), "beam", "HEA120", "c33")
+place_structural_element(model, Point.from_matrix(P1), Point.from_matrix(P2), "beam", "HEA120", "Steel", "C2")
 
 
 model.write("model1.ifc")
