56d7a7f65418d1cbbf480462e24327ffc3e19517
Maarten Vroegindeweij <30430941+DutchSailor@users.noreply.github.com>
2024-10-07 08:27:08 +0200

    Update

--- sandbox/Versie juni 2024/example/GIS2BIM/NL_3_Cadaster.py
+++ sandbox/Versie juni 2024/example/GIS2BIM/NL_3_Cadaster.py
@@ -1,9 +1,11 @@
 from packages.GIS2BIM.GIS2BIM_NL import *
+from packages.GIS2BIM.GIS2BIM import *
 from project.fileformat import *
 from exchange.GIS2BIM import *
 from geometry.mesh import MeshPB
 from library.material import *
 from abstract.image import *
+#from GIS2BIM_single_file import *
 
 # Description
 # This script creates lines for the cadastral parcels and a mesh for a building footprint for any building in
@@ -22,11 +24,11 @@ Centerline2 = ["Center Line 2", [8, 4, 4, 4], 500]  # Rule: line, whitespace, li
 # BASE VALUES
 RdX = lst[0]
 RdY = lst[1]
-Bbox = GIS2BIM.CreateBoundingBox(RdX, RdY, Bboxwidth, Bboxwidth, 0)
+Bbox = CreateBoundingBox(RdX, RdY, Bboxwidth, Bboxwidth, 0)
 
 # Aerialphoto
 fileLocationWMS = tempfolder + "luchtfoto_2020_2.png"
-a = GIS2BIM.WMSRequest(GIS2BIM.GetWebServerData("NL_PDOK_Luchtfoto_2020_28992", "webserverRequests",
+a = WMSRequest(GetWebServerData("NL_PDOK_Luchtfoto_2020_28992", "webserverRequests",
                                                 "serverrequestprefix"), Bbox, fileLocationWMS,
                        1500, 1500)
 
@@ -34,14 +36,14 @@ a = GIS2BIM.WMSRequest(GIS2BIM.GetWebServerData("NL_PDOK_Luchtfoto_2020_28992", 
 # GISProject.objects.append(img)
 
 # KADASTRALE GRENZEN
-curvesCadaster = GIS2BIM.PointsFromWFS(NLPDOKCadastreCadastralParcels, Bbox, NLPDOKxPathOpenGISposList, -RdX, -RdY,
+curvesCadaster = PointsFromWFS(NLPDOKCadastreCadastralParcels, Bbox, NLPDOKxPathOpenGISposList, -RdX, -RdY,
                                        1000, 2)
 # for i in WFSCurvesToBPCurvesLinePattern(curvesCadaster, Centerline):
 for i in WFSCurvesToBPCurves(curvesCadaster):
     GISProject.objects.append(i)
 
 # GEBOUWEN
-curvesBAG = GIS2BIM.PointsFromWFS(NLPDOKBAGBuildingCountour, Bbox, NLPDOKxPathOpenGISposList, -RdX, -RdY, 1000,
+curvesBAG = PointsFromWFS(NLPDOKBAGBuildingCountour, Bbox, NLPDOKxPathOpenGISposList, -RdX, -RdY, 1000,
                                   2)
 BPCurvesBAG = WFSCurvesToBPCurves(curvesBAG)
 # for i in WFSCurvesToBPCurves(curvesBAG):
